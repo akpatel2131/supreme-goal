3d16b5edea2681a347abb864ae47dbc7faedabeb
krunal <krunal@krunals-MacBook-Air.local>
2025-02-28 16:37:32 +0530

    adding top nav, hero section and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import './App.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { lazy, Suspense } from 'react';
+import TopNav from "./components/TopNav";
+import Footer from './components/Footer';
+const PerformanceSection = lazy(() => import('./components/PerformanceSection'));
+import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 function App() {
   return (
-    <div>hii</div>
+    <div className="flex flex-col min-h-screen">
+      <TopNav />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PerformanceSection />
+      </Suspense>
+      <Footer />
+    </div>
   );
 }
 
